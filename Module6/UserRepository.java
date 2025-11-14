8d498a2ee1c9c6ab85d2704f65ce0d48f0004a79
Nishant-Ishitaaa <ishitanishant68@gmail.com>
2025-11-14 15:11:45 +0530

    Delete Module6 directory

--- Module6/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.taxing.repo;
-
-import com.example.taxing.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
